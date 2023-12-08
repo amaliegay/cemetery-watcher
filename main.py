3effafe4a0f8b77583e60f20cea4f5339e08d352
Amalie <amaliegay@outlook.com>
2023-12-08 10:37:50 +0800

    draw background

--- main.py
+++ main.py
@@ -3,20 +3,41 @@ from os import listdir
 from os.path import isfile, join
 import random
 import math
+import numpy as np 
 
+# import pygame module 
 import pygame
 
 pygame.init()
 
-pygame.display.set_caption("SHOOTER")
+# screen size [width, height]
+WIDTH, HEIGHT = 600, 600
+SCREEN_SIZE = [WIDTH, HEIGHT]
 
+# setting the size of the window 
+window = pygame.display.set_mode(SCREEN_SIZE)
+
+# store the color
 WHITE = (255, 255, 255)
 BG_COLOR = WHITE
-WIDTH, HEIGHT = 600, 600
-FPS = 60
-PLAYER_VELOCITY = 5
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
+# set caption of screen 
+pygame.display.set_caption("SHOOTER")
+
+FPS = 60
+
+class Actor(pygame.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.bounding_box = pygame.Rect(x, y, width, height)
+        self.velocity = np.array([0,0]) 
+
+
+def draw(window, background):
+    # draw the background image
+    window.bilt(background, (0,0))
+    pygame.display.update()
 
 
 def get_background(name):
@@ -26,6 +47,8 @@ def get_background(name):
 
 def main(window):
     clock = pygame.time.Clock()
+
+    # get the background image
     background = get_background("cemetery.png")
 
     run = True
@@ -36,6 +59,9 @@ def main(window):
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw(background)
+
     pygame.quit()
     quit()
 
